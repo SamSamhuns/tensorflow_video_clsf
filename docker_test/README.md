d2ef41345fedafe2f6bfa0096b889ae63f79ff41
SamSamhuns <samhunsadamant@gmail.com>
2024-12-25 11:28:34 +0400

    Fix dkr readme & scripts

--- docker_test/README.md
+++ docker_test/README.md
@@ -3,7 +3,7 @@
 ## Build and Run Docker image.
 
 ```shell
-$ bash scripts/build_docker.sh
-$ bash scripts/run_docker.sh -h <EXPOSED_HTTP_PORT_NUM> -g <CUDA_GPU_DEVICE_NUMBER>
+bash scripts/build_docker.sh
+bash scripts/run_docker.sh -h <EXPOSED_HTTP_PORT_NUM> -g <CUDA_GPU_DEVICE_NUMBER>
 # Wait for model loading (60 seconds approx) and use the Fast API with Swagger UI to test.
 ```
--- docker_test/scripts/run_docker.sh
+++ docker_test/scripts/run_docker.sh
@@ -2,23 +2,21 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -g gpunumber -h http_port"
-   echo -e "\t-g gpunumber"
+   echo "Usage: $0 -h http_port"
    echo -e "\t-h http"
    exit 1 # Exit script after printing help
 }
 
-while getopts "g:h:" opt
+while getopts "h:" opt
 do
    case "$opt" in
-      g ) gpunumber="$OPTARG" ;;
       h ) http="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$gpunumber" ] || [ -z "$http" ]
+if [ -z "$http" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
@@ -27,7 +25,6 @@ fi
 echo "Running docker with exposed fastapi port: $http"
 docker run --rm -ti \
       --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
-      --gpus device="$gpunumber" \
       --name docker_api_test_container \
       -p $http:8080 \
       docker_api_test \
